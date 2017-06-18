a74476ca3ba1f43e6c7426624bf980f65ba2adbd
Karel Zavadil <mojemejly@centrum.cz>
2017-06-18 23:43:36 +0200

    menu

--- app/config/admin.php
+++ app/config/admin.php
@@ -3,10 +3,18 @@
 	return [
 		
 		'menu' => [
-			['admin/contexts','Contexts'],
-			['admin/origins','Origins'],
-			['admin/authors','Authors'],
-			['admin/quotes','Quotes']
+			['admin/products','Products'],
+			['admin/orders','Orders'],
+			['admin/customers','Customers'],
+			[
+				[
+					['admin/categories','Categories'],
+					['admin/currencies', 'Currencies'],
+					['admin/payment_types', 'Payment types'],
+					['admin/delivery_types', 'Delivery types']
+				],
+				'More...'
+			]
 		]
 		
 	];
