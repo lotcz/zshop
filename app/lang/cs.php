688a31fb297575339f4b04e6827ff37feed32afb
Karel Zavadil <mojemejly@centrum.cz>
2017-09-10 00:36:48 +0200

    translations

--- app/lang/cs.php
+++ app/lang/cs.php
@@ -1,107 +1,85 @@
 <?php
-	$language_data = [];
-	$language_data['language_code'] = 'cs';
-	$language_data['language_name'] = 'Čeština';
-	$language_data['Czech'] = 'Čeština';
-	$language_data['cs_css'] = 'active';
-	$language_data['thousands_separator'] = '&nbsp;';
-	$language_data['decimal_separator'] = ',';
-
-	$language_data['Hello'] = 'Dobrý den';
-	$language_data['Anonymous'] = 'Anonym';
-	$language_data['Log Out'] = 'Odhlásit se';
-	$language_data['Go to E-shop'] = 'Přejít do e-shopu';
-	$language_data['Forgotten Password'] = 'Zapomenuté heslo';
-	$language_data['Login or E-mail'] = 'Login nebo e-mail';
-	$language_data['Reset Password'] = 'Resetovat heslo';
-	$language_data['Confirm Password'] = 'Potvrdit heslo';
-	$language_data['Passwords don\'t match.'] = 'Hesla se neshodují.';
-	$language_data['Register'] = 'Registrovat';
-	$language_data['Order & Register'] = 'Objednat & registrovat';
-	$language_data['Registration'] = 'Registrace';
-	$language_data['Thank you for your registration'] = 'Děkujeme za Vaši registraci';
-	$language_data['Shopping Cart'] = 'Nákupní košík';
-	$language_data['Sign In'] = 'Přihlásit';
-	$language_data['Back'] = 'Zpět';
-	$language_data['Save'] = 'Uložit';
-	$language_data['Delete'] = 'Smazat';
-	$language_data['Edit'] = 'Editovat';
-	$language_data['Password'] = 'Heslo';
-	$language_data['Search'] = 'Hledat';
-
-	$language_data['Full name'] = 'Jméno a příjmení';
-
-	/* PRODUCTS AND CART */
-	$language_data['Buy'] = 'Koupit';
-	$language_data['Order by'] = 'Řadit podle';
-	$language_data['sortby_Price'] = 'Ceny - od nejlevnějších';
-	$language_data['sortby_Price_DESC'] = 'Ceny - od nejdražších';
-	$language_data['sortby_Alphabet'] = 'Abecedy - A až Z';
-	$language_data['sortby_Alphabet_DESC'] = 'Abecedy - Z až A';
-	$language_data['sortby_Popularity'] = 'Popularity';
-	$language_data['asc'] = 'Vzestupně';
-	$language_data['desc'] = 'Sestupně';
-	$language_data['Free'] = 'Zdarma';
-	$language_data['Price'] = 'Cena';
-	$language_data['Empty'] = 'Prázdný';
-	$language_data['Cart'] = 'Košík';
-	$language_data['<b>%s</b> items'] = '<b>%s</b> položek';
-	$language_data['Pick up in store'] = 'Osobní převzetí';
-	$language_data['Czech post'] = 'Česká Pošta';
-	$language_data['Parcel service'] = 'Kurýr';
-
-	/* LONG USER MESSAGES */
-	$language_data['--cookies--'] = 'Tato stránka nefunguje bez souborů cookies v prohlížeči. Prosím zapněte si funkci cookies v nastavení Vašeho prohlížeče.';
-	$language_data['--please-log-in--'] = 'Momentálně nejste přihlášeni. Pokud máte účet v našem obchodě, přihlašte se a dokončete nákup pod Vaším účtem.';
-	$language_data['--order-and-register--'] = 'Pokud u nás ještě nemáte účet, vytvoříme jej pro Vás v průběhu obědnávky.';
-	$language_data['--select-delivery--'] = 'Zvolte prosím, jakým si způsobem si přejete doručit Vaše zboží.';
-
-	/* ADMIN SECTION */
-	$language_data['Administrator Sign In'] = 'Vstup pro správce';
-	$language_data['Max value is %s.'] = 'Maximální hodnota je %s.';
-	$language_data['%d - %d of %d'] = '<b>%d</b> až <b>%d</b> z celkových <b>%d</b>.';
-
-	$language_data['Administration'] = 'Administrace';
-	$language_data['Dashboard'] = 'Ovládací panel';
-	$language_data['Administrators'] = 'Správci';
-	$language_data['Editing Administrator'] = 'Editace správce';
-	$language_data['Add Administrator'] = 'Přidat správce e-shopu';
-	$language_data['Last Visit'] = 'Poslední návštěva';
-	$language_data['Failed Logins'] = 'Neúspěsná přihlášení';
-
-	$language_data['Products'] = 'Zboží';
-	$language_data['Editing Product'] = 'Editace zboží';
-	$language_data['New Product'] = 'Nové zboží';
-
-	$language_data['Categories'] = 'Kategorie';
-	$language_data['Editing Category'] = 'Editace kategorie';
-	$language_data['New Category'] = 'Nová kategorie';
-
-	$language_data['Customers'] = 'Zákazníci';
-	$language_data['Editing Customer'] = 'Editace zákazníka';
-	$language_data['New Customer'] = 'Nový zákazník';
-
-	$language_data['Currency'] = 'Měna';
-	$language_data['Currencies'] = 'Měny';
-
-	$language_data['Delivery Type'] = 'Způsob dopravy';
-	$language_data['Delivery Types'] = 'Způsoby dopravy';
-
-	$language_data['Payment Type'] = 'Způsob platby';
-	$language_data['Payment Types'] = 'Způsoby platby';
-
-	$language_data['Place an order'] = 'Vytvořit objednávku';
-
-	/* FORM VALIDATIONS */
-	$language_data['Value must be at least %s characters long.'] = 'Hodnota musí být alespoň %s znaků dlouhá.';
-	$language_data['Please enter valid e-mail address.'] = 'Zadejte prosím platnou emailovou adresu.';
-
-	$language_data['Total'] = 'Celkem';
-	$language_data['Total <b>%s</b> items.'] = 'Celkem <b>%s</b> položek.';
-
-	/* EMAILS */
-	$language_data['Dear customer'] = 'Vážený zákazníku';
-	$language_data['Your order n. %s has been accepted.'] = 'Vaše objednávka č. %s byla přijata.';
-	$language_data['If you forget your password, you can reset it <a href="%s">here</a>.'] = 'Pokud zapomenete Vaše heslo, můžete jej resetovat <a href="%s">zde</a>.';
-
-	return $language_data;
+$language_data = [];
+$language_data['language_code'] = 'cs';
+$language_data['language_name'] = 'Čeština';
+$language_data['Czech'] = 'Čeština';
+$language_data['cs_css'] = 'active';
+$language_data['thousands_separator'] = '&nbsp;';
+$language_data['decimal_separator'] = ',';
+$language_data['Hello'] = 'Dobrý den';
+$language_data['Anonymous'] = 'Anonym';
+$language_data['Log Out'] = 'Odhlásit se';
+$language_data['Go to E-shop'] = 'Přejít do e-shopu';
+$language_data['Forgotten Password'] = 'Zapomenuté heslo';
+$language_data['Login or E-mail'] = 'Login nebo e-mail';
+$language_data['Reset Password'] = 'Resetovat heslo';
+$language_data['Confirm Password'] = 'Potvrdit heslo';
+$language_data['Passwords don\'t match.'] = 'Hesla se neshodují.';
+$language_data['Register'] = 'Registrovat';
+$language_data['Order & Register'] = 'Objednat & registrovat';
+$language_data['Registration'] = 'Registrace';
+$language_data['Thank you for your registration'] = 'Děkujeme za Vaši registraci';
+$language_data['Shopping Cart'] = 'Nákupní košík';
+$language_data['Sign In'] = 'Přihlásit';
+$language_data['Back'] = 'Zpět';
+$language_data['Save'] = 'Uložit';
+$language_data['Delete'] = 'Smazat';
+$language_data['Edit'] = 'Editovat';
+$language_data['Password'] = 'Heslo';
+$language_data['Search'] = 'Hledat';
+$language_data['Full name'] = 'Jméno a příjmení';
+$language_data['Buy'] = 'Koupit';
+$language_data['Order by'] = 'Řadit podle';
+$language_data['sortby_Price'] = 'Ceny - od nejlevnějších';
+$language_data['sortby_Price_DESC'] = 'Ceny - od nejdražších';
+$language_data['sortby_Alphabet'] = 'Abecedy - A až Z';
+$language_data['sortby_Alphabet_DESC'] = 'Abecedy - Z až A';
+$language_data['sortby_Popularity'] = 'Popularity';
+$language_data['asc'] = 'Vzestupně';
+$language_data['desc'] = 'Sestupně';
+$language_data['Free'] = 'Zdarma';
+$language_data['Price'] = 'Cena';
+$language_data['Empty'] = 'Prázdný';
+$language_data['Cart'] = 'Košík';
+$language_data['<b>%s</b> items'] = '<b>%s</b> položek';
+$language_data['Pick up in store'] = 'Osobní převzetí';
+$language_data['Czech post'] = 'Česká Pošta';
+$language_data['Parcel service'] = 'Kurýr';
+$language_data['--cookies--'] = 'Tato stránka nefunguje bez souborů cookies v prohlížeči. Prosím zapněte si funkci cookies v nastavení Vašeho prohlížeče.';
+$language_data['--please-log-in--'] = 'Momentálně nejste přihlášeni. Pokud máte účet v našem obchodě, přihlašte se a dokončete nákup pod Vaším účtem.';
+$language_data['--order-and-register--'] = 'Pokud u nás ještě nemáte účet, vytvoříme jej pro Vás v průběhu objednávky.';
+$language_data['--select-delivery--'] = 'Zvolte prosím, jakým si způsobem si přejete doručit Vaše zboží.';
+$language_data['Administrator Sign In'] = 'Vstup pro správce';
+$language_data['Max value is %s.'] = 'Maximální hodnota je %s.';
+$language_data['%d - %d of %d'] = '<b>%d</b> až <b>%d</b> z celkových <b>%d</b>.';
+$language_data['Administration'] = 'Administrace';
+$language_data['Dashboard'] = 'Ovládací panel';
+$language_data['Administrators'] = 'Správci';
+$language_data['Editing Administrator'] = 'Editace správce';
+$language_data['Add Administrator'] = 'Přidat správce e-shopu';
+$language_data['Last Visit'] = 'Poslední návštěva';
+$language_data['Failed Logins'] = 'Neúspěsná přihlášení';
+$language_data['Products'] = 'Zboží';
+$language_data['Editing Product'] = 'Editace zboží';
+$language_data['New Product'] = 'Nové zboží';
+$language_data['Categories'] = 'Kategorie';
+$language_data['Editing Category'] = 'Editace kategorie';
+$language_data['New Category'] = 'Nová kategorie';
+$language_data['Customers'] = 'Zákazníci';
+$language_data['Editing Customer'] = 'Editace zákazníka';
+$language_data['New Customer'] = 'Nový zákazník';
+$language_data['Currency'] = 'Měna';
+$language_data['Currencies'] = 'Měny';
+$language_data['Delivery Type'] = 'Způsob dopravy';
+$language_data['Delivery Types'] = 'Způsoby dopravy';
+$language_data['Payment Type'] = 'Způsob platby';
+$language_data['Payment Types'] = 'Způsoby platby';
+$language_data['Place an order'] = 'Vytvořit objednávku';
+$language_data['Value must be at least %s characters long.'] = 'Hodnota musí být alespoň %s znaků dlouhá.';
+$language_data['Please enter valid e-mail address.'] = 'Zadejte prosím platnou emailovou adresu.';
+$language_data['Total'] = 'Celkem';
+$language_data['Total <b>%s</b> items.'] = 'Celkem <b>%s</b> položek.';
+$language_data['Dear customer'] = 'Vážený zákazníku';
+$language_data['Your order n. %s has been accepted.'] = 'Vaše objednávka č. %s byla přijata.';
+$language_data['If you forget your password, you can reset it <a href="%s">here</a>.'] = 'Pokud zapomenete Vaše heslo, můžete jej resetovat <a href="%s">zde</a>.';
+return $language_data;
--- app/lang/en.php
+++ app/lang/en.php
@@ -1,20 +1,17 @@
 <?php
-	global $language_data;
-	$language_data['language_code'] = 'en';
-	$language_data['language_name'] = 'English';
-	$language_data['en_css'] = 'active';
-	$language_data['thousands_separator'] = ',';
-	$language_data['decimal_separator'] = '.';
-
-	/* PRODUCTS */
-	$language_data['sortby_Price'] = 'Price';
-	$language_data['sortby_Alphabet'] = 'Alphabet';
-	$language_data['sortby_Popularity'] = 'Popularity';
-	$language_data['asc'] = 'Ascending';
-	$language_data['desc'] = 'Descending';
-
-	/* LONG USER MESSAGES */
-	$language_data['--cookies--'] = 'This site cannot be used without browser cookies! Please enable cookies in your browser settings.';
-	$language_data['--please-log-in--'] = 'You are currently not logged in. If you have an account with our e-shop, log in to finish your shopping under your account.';
-	$language_data['--order-and-register--'] = 'If you don\'t have an account with us yet, we will create one for you in course of order process.';
-	$language_data['--select-delivery--'] = 'Please select how you wish your goods delivered to you.';
+$language_data = [];
+$language_data['--cookies--'] = 'This site cannot be used without browser cookies! Please enable cookies in your browser settings.';
+$language_data['--order-and-register--'] = 'If you don\'t have an account with us yet, we will create one for you in course of order process.';
+$language_data['--please-log-in--'] = 'You are currently not logged in. If you have an account with our e-shop, log in to finish your shopping under your account.';
+$language_data['--select-delivery--'] = 'Please select how you wish your goods delivered to you.';
+$language_data['asc'] = 'Ascending';
+$language_data['decimal_separator'] = '.';
+$language_data['desc'] = 'Descending';
+$language_data['en_css'] = 'active';
+$language_data['language_code'] = 'en';
+$language_data['language_name'] = 'English';
+$language_data['sortby_Alphabet'] = 'Alphabet';
+$language_data['sortby_Popularity'] = 'Popularity';
+$language_data['sortby_Price'] = 'Price';
+$language_data['thousands_separator'] = ',';
+return $language_data;
