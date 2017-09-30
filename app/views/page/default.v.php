5577214fcd8cdfa6b680455b03ec44f2b71266aa
Karel Zavadil <mojemejly@centrum.cz>
2017-09-30 16:10:52 +0200

    git

--- app/views/page/default.v.php
+++ app/views/page/default.v.php
@@ -1,11 +1,11 @@
-<div class="inner cover">	
+<div class="inner cover">
 	<div class="panel panel-primary">
 		<div class="panel-heading">
 			<h3 class="panel-title">Panel title</h3>
 		</div>
 		<div class="panel-body">
-			Hello World!
+			Hello World! This is GIT updated.
 		</div>
 	</div>
-	
+
 </div>
