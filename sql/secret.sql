e8aee791116cafea29f761417a767813bc4ceda0
Karel Zavadil <mojemejly@centrum.cz>
2017-09-30 15:40:27 +0200

    s

--- sql/secret.sql
+++
@@ -1,2 +0,0 @@
-USE zshop;
-INSERT INTO `users` (`user_login`, `user_email`, `user_password_hash`, `user_is_superuser`, `user_language_id`) VALUES ( 'karel', 'mojemejly@centrum.cz', '$2y$10$bhLm9lallISZPBloadZvH.NgvbGnijLJCvRAWkIlYNzycdTWI2w4S', 1, 1); /*karel123*/
