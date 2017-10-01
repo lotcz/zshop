e7323cd2f6d894da95da7226eb838d59ef4beb68
Karel Zavadil <mojemejly@centrum.cz>
2017-10-01 22:28:46 +0200

    1.05

--- app/config/app.php
+++ app/config/app.php
@@ -4,7 +4,7 @@
 
 		// this is application version
 		// should be identical with GIT branch name
-		'version' => 1.04,
+		'version' => 1.05,
 
 		// this is minimum required zEngine version
 		// should be identical with GIT branch name
