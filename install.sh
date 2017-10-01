867aa4523aeec8df537a8ca0236fd9b1f2fa0212
Karel Zavadil <mojemejly@centrum.cz>
2017-10-01 18:43:11 +0200

    cr

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ git clone https://github.com/lotcz/zshop.git zShop
 git clone https://github.com/lotcz/zEngine.git zEngine
 
 cd zEngine
-chmod +x upgrade
-chmod +x update
-./upgrade 1.02
+chmod +x upgrade.sh
+chmod +x update.sh
+./upgrade.sh 1.02
 
 cd ..
 cd zShop
-chmod +x upgrade
-chmod +x update
-./upgrade 1.03
+chmod +x upgrade.sh
+chmod +x update.sh
+./upgrade.sh 1.03
 
 
 
