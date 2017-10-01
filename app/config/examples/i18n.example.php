5fcfd3a71c2ed9cce2682d56389dd0b7154b9566
Karel Zavadil <mojemejly@centrum.cz>
2017-10-01 23:40:07 +0200

    cleaning

---
+++ app/config/examples/i18n.example.php
@@ -0,0 +1,9 @@
+<?php
+
+	return [
+		//absolute path to directory with language data files
+		'localization_dir' => __DIR__ . '/../lang/',
+
+		//default language which will be selected if automatic selection fails
+		'default_language' => 'cs',
+	];
