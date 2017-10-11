fcb3fb4833e7667d3e4cf4d8914323933645b9eb
Karel Zavadil <mojemejly@centrum.cz>
2017-10-11 21:28:04 +0200

    piwik

--- app/views/master/default.v.php
+++ app/views/master/default.v.php
@@ -50,5 +50,19 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<script src="<?=$this->url('js/zshop.js')?>"></script>
 
+		<!-- Piwik -->
+		<script type="text/javascript">
+		  var _paq = _paq || [];
+		  _paq.push(['trackPageView']);
+		  _paq.push(['enableLinkTracking']);
+		  (function() {
+		    _paq.push(['setTrackerUrl',  'http://api-df849de5502297c3c10e0443aa09ec2c.sandstorm.zavadil.eu:6080']);
+		    _paq.push(['setSiteId', 1]);
+		    _paq.push(['setApiToken', 'qbjZLVcnaA712qoyJcfP0tRXPhO7l-BWwINkHM_ea2f']);
+		    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		    g.type='text/javascript'; g.async=true; g.defer=true; g.src='http://sqbofm0lxvgavecxzcop.sandstorm.zavadil.eu:6080/embed.js'; s.parentNode.insertBefore(g,s);
+		  })();
+		</script>
+		<!-- End Piwik Code -->
 	</body>
 </html>
